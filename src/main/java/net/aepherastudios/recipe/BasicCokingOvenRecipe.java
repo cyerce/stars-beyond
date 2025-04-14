4c5fdd4156d89dd09c3e6e43e1137a72e193bb33
MENTIY <164089516+MENTIY@users.noreply.github.com>
2025-04-13 18:09:22 -0600

    The setup for geting the resipies and inner goings on with the basic coking oven
    Signed-off-by: MENTIY <164089516+MENTIY@users.noreply.github.com>

---
+++ src/main/java/net/aepherastudios/recipe/BasicCokingOvenRecipe.java
@@ -0,0 +1,118 @@
+package net.aepherastudios.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.aepherastudios.starsbeyond.StarsBeyond;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+public class BasicCokingOvenRecipe implements Recipe<SimpleContainer> {
+    private final NonNullList<Ingredient> inputItems;
+    private final ItemStack output;
+    private final ResourceLocation id;
+
+    public BasicCokingOvenRecipe(ResourceLocation id, ItemStack output, NonNullList<Ingredient> inputItems){
+        this.inputItems = inputItems;
+        this.output = output;
+        this.id = id;
+    }
+
+    @Override
+    public boolean matches(SimpleContainer sContainer, Level level) {
+        if(level.isClientSide()){
+            return false;
+        }
+        return inputItems.get(0).test(sContainer.getItem(0));
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer simpleContainer, RegistryAccess registryAccess) {
+        return output.copy();
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int i, int i1) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess registryAccess) {
+        return null;
+    }
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        return this.inputItems;
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return null;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static class Type implements RecipeType<BasicCokingOvenRecipe>{
+        private Type() {}
+
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "basic_coking_oven";
+    }
+
+    public static class Serializer implements RecipeSerializer<BasicCokingOvenRecipe>{
+        public static final Serializer INSTANCE = new Serializer();
+        public static final ResourceLocation ID =
+                new ResourceLocation(StarsBeyond.MOD_ID, "basic_coking_oven");
+
+        @Override
+        public BasicCokingOvenRecipe fromJson(ResourceLocation id, JsonObject json) {
+            ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "output"));
+
+            JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
+            NonNullList<Ingredient> inputs = NonNullList.withSize(1, Ingredient.EMPTY);
+
+            for(int i = 0; i < inputs.size(); i++){
+                inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
+            }
+            return new BasicCokingOvenRecipe(id, output, inputs);
+        }
+
+        @Override
+        public @Nullable BasicCokingOvenRecipe fromNetwork(ResourceLocation id, FriendlyByteBuf buf) {
+            NonNullList<Ingredient> inputs = NonNullList.withSize(buf.readInt(), Ingredient.EMPTY);
+
+            for(int i = 0; i < inputs.size(); i++){
+                inputs.set(i, Ingredient.fromNetwork(buf));
+            }
+
+            ItemStack output = buf.readItem();
+            return new BasicCokingOvenRecipe(id, output, inputs);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buf, BasicCokingOvenRecipe recipe) {
+            buf.writeInt(recipe.getIngredients().size());
+
+            for(Ingredient ing : recipe.getIngredients()){
+                ing.toNetwork(buf);
+            }
+            buf.writeItemStack(recipe.getResultItem(null), false);
+        }
+    }
+}
